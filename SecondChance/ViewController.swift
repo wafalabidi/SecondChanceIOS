e7b6c888f58ba92bbace3083dab1685896acc8c3
Wafe <Wafee@macbook-air-de-wafe.local>
2018-03-12 18:32:24 +0100

    Initial Commit

---
+++ SecondChance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondChance
+//
+//  Created by macbook on 12/03/2018.
+//  Copyright © 2018 Wafe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
